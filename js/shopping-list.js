47e1c6ed21f02b8c9185d4c32e1e1fce1255605f
Bruce P Cox Jr <bpcoxjr@yahoo.com>
2015-08-22 16:02:16 -0400

    debugged JS

--- js/shopping-list.js
+++ js/shopping-list.js
@@ -1,49 +1,46 @@
-$(document).ready(function(){
-	$(addListItem)
+function addListItem() {
 	var write = $('#newItem').val();
 	var list = $('#itemList');
-	var item = $('<li><button class="check-mark">&#10003;</button><span class="list">' + write + '</span><<button class="delete-item">&#10005;</button></li>');
-	if(write.length === 0 ||write.length > 25) {
-	return false;
-});
+	var item = $('<li><button class="check-mark">&#10003;</button><span class="list">' + write + '</span><button class="delete-item">&#10005;</button></li>');
 
-/*When item is added it is placed at top of the list*/
-$(document).ready(function{
-	(list).prepend(item);
+	if(write.length === 0 || write.length > 25) {
+		return false;
+	}
+	/*When item is added it is placed at top of the list*/
+	list.prepend(item);
 	$(newItem).val('');
-});	
+}
 
-$(document).ready(function(){
+function deleteItem(){
 	$(this).parent().remove();
-});
+}
 
 /*Allows user to tick item off list, highlighting*/
-$(document).ready(function(){
+function tickItem(){
 	$(this).parent().toggleClass('tick-off');
+	/*Once ticked, item  moves to bottom of list*/	
+	$('#itemList').find('.tick-off').appendTo('#itemList');	
+}
 
-/*Once ticked, item  moves to bottom of list*/
-$(document).ready(function(){	
-	$('#itemList').find('.tick-off').appendTo('#itemList');
-});	
+
 
 /*Clears all items from list when user clicks 'clear list' button*/
-$(document).ready(function(){
 	$(document).on('click', '#resetButton', function(){
 		$('#itemList').empty();
 	});
-});
 
 /*Allows user to add items to list*/
-$(document).ready(function(){
-		var add = $('addItem');
+$(function(){
+		var add = $('#addItem');
 		var newItem = $('#newItem');
-		var list = $('itemList');
+		var list = $('#itemList');
 
 		add.on('click', addListItem);
 		list.on('click', '.check-mark', tickItem);
-		list.on('click', '.delete', deleteItem);
+		list.on('click', '.delete-item', deleteItem);
 		newItem.on('keypress', function(e){
 			if(e.which == 13) {
 				addListItem();
-			}		
+			}
+		});		
 });
